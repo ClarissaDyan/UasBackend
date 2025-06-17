f5edcec9d3e7d8cdf845e497ec52802a2657bb70
ClarissaDyan <clarissa.535240118@stu.untar.ac.id>
2025-06-17 19:38:38 +0700

    tambahin bagian edit

--- resources/views/penghuni/edit.blade.php
+++ resources/views/penghuni/edit.blade.php
@@ -1,31 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="page-section portfolio" id="tambah">
-    <div class="container">
-        <h1>Edit Data Penghuni</h1>
+<div class="container">
+    <h2>Edit Data Penghuni</h2>
 
-        <form action="{{ route('penghuni.update', $phi->id) }}" method="POST">
-            @method('PUT')
-            @csrf
-            <div class="mb-3">
-                <label for="nama" class="form-label">Nama</label>
-                <input type="text" class="form-control" id="nama" name="nama" required>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Nomor</label>
-                <input type="text" class="form-control" id="nomor" name="nomor" required>
-            </div>
-            <div class="mb-3">
-                <label for="jurusan" class="form-label">Kamar</label>
-                <input type="text" class="form-control" id="kamar" name="kamar" required>
-            </div>
-            <div class="mb-3">
-                <label for="jurusan" class="form-label">Masa Sewa</label>
-                <input type="text" class="form-control" id="masaSewa" name="masaSewa" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-</section>
-@endsection 
+    <form method="POST" action="{{ route('penghuni.update', $phi->id) }}">
+        @csrf
+        @method('PUT')
+
+        <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama" value="{{ $phi->nama }}">
+        </div>
+
+        <div class="form-group">
+            <label for="nomor">Nomor</label>
+            <input type="text" class="form-control" id="nomor" name="nomor" value="{{ $phi->nomor }}">
+        </div>
+
+        <div class="form-group">
+            <label for="kamar">Kamar</label>
+            <input type="text" class="form-control" id="kamar" name="kamar" value="{{ $phi->kamar }}">
+        </div>
+
+        <div class="form-group">
+            <label for="masaSewa">Masa Sewa</label>
+            <input type="text" class="form-control" id="masaSewa" name="masaSewa" value="{{ $phi->masaSewa }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary mt-3">Submit</button>
+    </form>
+</div>
+@endsection
